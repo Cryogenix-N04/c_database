f3e39ba63f1868156026f5f4c3f6eddd2651ad6a
Het Pandya <hetpandyaedu1529@gmail.com>
2026-04-06 20:07:22 -0400

    KV Put Commit 1

--- src/kv.c
+++ src/kv.c
@@ -1,4 +1,71 @@
 #include <kv.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TOMBSTONE 0x1
+
+size_t hash(char *val, int capacity) {
+    size_t hash = 0x13371337deadbeef;
+
+    while (*val) {
+        hash ^= *val;
+        hash = hash << 8;
+        hash += *val;
+
+        val++;
+    }
+
+    return hash % capacity;
+}
+
+// fn kv_put
+// params:
+//  - db:   a pointer to the db
+//  - key:  a pointer to the key value
+//  - value: a pointer to the value itself
+//  returns the index of the key, otherwise on
+//  error, returns -1, on not found returns -2
+int kv_put(kv_t *db, char *key, char *value) {
+    if(!db || !key || !value) return -1;
+
+    size_t index = hash(key, db->capacity);
+
+    for (int i = 0; i < db->capacity - 1; i++) {
+
+        size_t real_index = (index + 1) % db->capacity;
+
+        kv_entry_t *entry = &db->entries[real_index];
+
+        // the key is already set, updating
+        if (entry->key &&
+            entry->key != (void *) TOMBSTONE &&
+            !strcmp(entry->key, key)) {
+                char *newval = strdup(value);
+                if (!newval) return -1;
+                entry->value = newval;
+            return real_index;
+        }
+
+        // land in a slot that is "empty"
+        // null or tombstone
+        if (!entry->key || entry->key == (void *) TOMBSTONE) {
+            char *newval = strdup(value);
+            char *newkey = strdup(key);
+            if (!newval || !newkey) {
+                free(newkey);
+                free(newval);
+                return -1;
+            }
+            entry->value = newval;
+            entry->key = newkey;
+            db->count++;
+            return real_index;
+        }
+    }
+
+    // the db is occupied
+    return -2;
+}
 
 kv_t *kv_init(size_t capacity) {
     if(capacity == 0) return NULL;
